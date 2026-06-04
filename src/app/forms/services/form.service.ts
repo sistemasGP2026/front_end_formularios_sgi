77482d393b081e9770b22c0345aae18c4983956a
sistemasGP2026 <sistemas@gestionpharma.com.co>
2026-06-04 16:09:46 -0500

    url de backend corregido

--- src/app/forms/services/form.service.ts
+++ src/app/forms/services/form.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class FormService {
-  protected readonly baseUrl: string = "http://localhost:3000";
+  protected readonly baseUrl: string = "http://192.168.1.103:2000";
   private http = inject(HttpClient);
 
   private getToken(): string {
